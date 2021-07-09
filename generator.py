314fa8f3c4ea49efff4922438f00d1fa6afb3dff
hunbL <hunbl@naver.com>
2021-07-09 14:52:41 +0900

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,9 +2,8 @@
 
 from transformers import pipeline
 
-# 모델과 태스트만 주어지면 바로 실행
+# 모델과 태스크만 주어지면 바로 실행
 nlp = pipeline("fill-mask", model="deeq/delectra-generator")
-#text = "서울은 한국의 [MASK] 입니다."
 text = "대한민국은 민주 [MASK] 입니다."
 result = nlp(text)
 
